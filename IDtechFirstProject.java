3903a4774e8c3ea693b2f5fede0ba1ee55e56ca6
jayd628 <91217296+jayd628@users.noreply.github.com>
2022-07-21 15:54:46 -0400

    Create IDtechFirstProject.java

---
+++ IDtechFirstProject.java
@@ -0,0 +1,20 @@
+package robothead;
+
+import processing.core.PApplet;
+
+
+public class RobotHead extends PApplet {
+
+	public static void main (String[]args) {
+		PApplet.main("robothead.RobotHead");
+	}
+	public void setting() {
+		size(700, 800);
+	}
+	public void setup() {
+		background(255, 255, 255);
+	}
+
+	public void draw() {
+	}
+}
