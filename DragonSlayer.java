7109388deebf0f51986c699c22e94f031b98b847
jayd628 <91217296+jayd628@users.noreply.github.com>
2022-07-21 16:10:27 -0400

    Create DragonSlayer.java

---
+++ DragonSlayer.java
@@ -0,0 +1,18 @@
+public class DragonSlayer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int playersStrength = 5;
+		int dragonsHP =35;
+		boolean hasSword=true;
+		if (playersStrength>dragonsHP) {
+			System.out.println("You defeated the dragon!");
+		}
+		else if (hasSword) { 
+			System.out.println("You're not strong enough to defeat the dragon. However, you remember your magic sword and use it to slay the beast.");
+	   }
+	   else  {
+			System.out.println("The dragon defeated you!");
+		}
+	}
+}
