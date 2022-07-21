0217484f0361cb77350fea4651d84b2eed14d6a1
jayd628 <91217296+jayd628@users.noreply.github.com>
2022-07-21 16:06:34 -0400

    Create ArithmeticExample.java

---
+++ ArithmeticExample.java
@@ -0,0 +1,15 @@
+public class ArithmeticExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int numberOne=8;
+		int theBestNumber=100;
+		numberOne+=10;
+		numberOne--;
+		theBestNumber*=numberOne;
+		theBestNumber++;
+		System.out.println("NumberOne:"+numberOne);
+		System.out.println("the best number:"+theBestNumber);
+	}
+
+}
