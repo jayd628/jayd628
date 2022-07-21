038524b275150b97e5d38f3f9336fc11041f03a0
jayd628 <91217296+jayd628@users.noreply.github.com>
2022-07-21 16:08:46 -0400

    Create CharacterPrinter.java

---
+++ CharacterPrinter.java
@@ -0,0 +1,14 @@
+public class CharacterPrinter {
+
+	public static void main(String[] args) {
+		int level;
+		level=1;
+		System.out.println("Level: "+level);
+		double health;
+		health=9.0;
+		System.out.println("Health:"+ health);
+		String name;
+		name="Mallis";
+		System.out.println("Character Name:"+name);
+	}
+}
