4fc9f232993adcd378bb890a8ab62cfc90d38c50
jayd628 <91217296+jayd628@users.noreply.github.com>
2022-07-21 16:23:28 -0400

    Create Rogue.java

---
+++ Rogue.java
@@ -0,0 +1,27 @@
+public class Rogue extends Character {
+	
+	public int dexterity= 20;
+	
+	public Rogue() {
+		super();
+		this.strength=8;
+		this.defense=4;
+		this.health=100;
+	}
+	
+	public int attack(Character target){
+        boolean criticalHit = Arena.generator.nextInt(100) < dexterity;
+        int damage = this.strength * 2;
+        if(criticalHit){
+            damage *= 2;
+            System.out.println("*** Critical Hit ***");
+        }
+        int damageDealt = target.takeDamage(damage);
+        return damageDealt; 
+        
+	}   
+
+	
+}
+
+
